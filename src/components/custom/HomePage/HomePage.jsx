2f4f11562394aec61cef49efcdaea1ae59e0c7bd
p1yux <soulpiyush09@gmail.com>
2024-11-10 10:16:02 +0530

    initial changes

--- src/components/custom/HomePage/HomePage.jsx
+++ src/components/custom/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState} from 'react'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 import hawamehal from '@/assets/images/hawamehal.jpeg'
